80920d99707b17e54e32c38a1dd122a1bf1360e0
Marko <mmarkom0001@gmail.com>
2019-05-27 10:05:19 +0200

    Doradio interface, ubacio stvari vezane za socket-e, napravio jos po neke male promene.

---
+++ RA5013AT-WAR/src/socketstuff/WebSocketServerEndpoint.java
@@ -0,0 +1,27 @@
+package socketstuff;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/webendpoint")
+public class WebSocketServerEndpoint {
+	
+	@OnOpen
+	public void onOpen() {	
+		System.out.println("WebSocketSeverEndpoint je otvoren! ");
+	}
+	
+	@OnMessage
+	public void onMessage(String message) {
+		System.out.println("Ja sam ovo dobio: " + message);
+	}
+	
+	@OnClose
+	public void onClose() {	
+		System.out.println("Zatvorio se WebSocketSeverEndpoint.");
+	}
+	
+}
